210b86241a08d19787be26f83c6390f46e931e3e
Avani Panchal <avanip.net@gmail.com>
2020-04-05 16:02:26 -0500

    initial commit

---
+++ AProperty/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AProperty
+{
+    abstract class AbstractProperty
+    {
+        abstract public string Name
+        {
+            get;
+            set;
+        }
+    }
+    class class2 : AbstractProperty
+    {
+        string name;
+        public override string Name 
+        { 
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine("name = " +name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class2 c2 = new class2();
+            c2.Name = "Abstract Property";
+            c2.display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SProperty/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SProperty
+{
+    class StaticProperty
+    {
+        static string name;
+        public static string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine("name = " + name);
+        }
+    }
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            StaticProperty s = new StaticProperty();
+            StaticProperty.Name = "Static Property";
+            s.display();
+            Console.ReadLine();
+        }
+    }
+}
