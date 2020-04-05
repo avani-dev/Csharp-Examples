27045c621c3173b56215176e5259060a874bc145
Avani Panchal <avanip.net@gmail.com>
2020-04-05 15:20:49 -0500

    initial commit

---
+++ EventDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegate
+{
+    public delegate void Mydel1();
+    class class1
+    {
+        public event Mydel1 Click;
+        public void Click_Method()
+        {
+            Console.WriteLine("Click functionality access");
+        }
+        public void display()
+        {
+            Click += new Mydel1(Click_Method);
+            Click();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class1 c1 = new class1();
+            c1.display();
+            Console.ReadLine();
+        }
+    }
+}
