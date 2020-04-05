866b28e8f3472d0e74120df009affbb9ad65776f
Avani Panchal <avanip.net@gmail.com>
2020-04-05 11:22:32 -0500

    initial commit

---
+++ AbstractSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractSample
+{
+    abstract class Employee
+    {
+        public void print()
+        {
+            Console.WriteLine("Non-abstract method");
+        }
+        public abstract void display();
+    }
+
+    class Mohit : Employee
+    {
+        public override void display()
+        {
+            Console.WriteLine("Abstract Class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mohit m1 = new Mohit();
+            m1.print();
+            m1.display();
+            Console.ReadLine();
+        }
+    }
+}
