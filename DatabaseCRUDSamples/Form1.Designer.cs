a0ffbcc7b0f86c6a6d35a1e1cc582178f4cc71c9
Avani Panchal <avanip.net@gmail.com>
2020-04-07 10:33:03 -0500

    Database insert, update and delete

--- DatabaseCRUDSamples/Form1.Designer.cs
+++ DatabaseCRUDSamples/Form1.Designer.cs
@@ -110,6 +110,7 @@
             this.button2.TabIndex = 7;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -119,6 +120,7 @@
             this.button3.TabIndex = 8;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox3
             // 
--- DatabaseCRUDSamples/Form1.cs
+++ DatabaseCRUDSamples/Form1.cs
@@ -69,5 +69,38 @@ namespace DatabaseCRUDSamples
             }
             con.Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Initial Catalog=sample;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand("Update csharp set id=@id, name=@name, address=@address where id =" +Convert.ToInt32(textBox4.Text)+"", con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@id", Convert.ToInt32(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@address", textBox3.Text);
+
+            int i = cmd.ExecuteNonQuery();
+            if(i>0)
+            {
+                MessageBox.Show("Record is Updated");
+            }
+            con.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Initial Catalog=sample;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand("Delete from csharp where id =" + Convert.ToInt32(textBox4.Text) + "", con);
+            con.Open();
+
+            int i = cmd.ExecuteNonQuery();
+            if(i>0)
+            {
+                MessageBox.Show("Record is Deleted");
+            }
+            con.Close();
+
+        }
     }
 }
