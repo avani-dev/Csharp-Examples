1a7d9e54a294a3dfb76617dff038a30acec5331c
Avani Panchal <avanip.net@gmail.com>
2020-04-07 08:55:10 -0500

    Database CRUD Sample

---
+++ DatabaseCRUDSamples/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DatabaseCRUDSamples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Initial Catalog=Sample;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand("Select * from cSharp", con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                //comboBox1.Items.Add(dr["id"].ToString());
+                //textBox1.Text = dr[0].ToString();
+                //textBox2.Text = dr[1].ToString();
+                //textBox3.Text = dr[2].ToString();
+
+            }
+            con.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Initial Catalog=Sample;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand("Select * from cSharp where id =" + Convert.ToInt32(textBox3.Text)+"", con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                //comboBox1.Items.Add(dr["id"].ToString());
+                textBox1.Text = dr[0].ToString();
+                textBox2.Text = dr[1].ToString();
+                textBox3.Text = dr[2].ToString();
+            }
+            con.Close();
+        }
+    }
+}
