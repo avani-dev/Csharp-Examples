899fe02a91213b5606b0e127985ca1c043848f0c
Avani Panchal <avanip.net@gmail.com>
2020-04-07 20:15:04 -0500

    Exception Handling Samples

---
+++ ExceptionHandlingSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingSample
+{
+    class MyExecption : Exception
+    {
+        public MyExecption(String str)
+        {
+            Console.WriteLine("User defined Exception");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception("Avani");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception caught here=" + ex.ToString());
+            }
+            Console.WriteLine("Last Statement");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExceptionHandlingSample2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingSample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int div = 0;
+            try
+            {
+                div = 100 / x;
+                Console.WriteLine("This line is not executed");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Result is =" +div);
+            Console.ReadLine();
+                
+        }
+    }
+}
