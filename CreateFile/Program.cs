1808f34b2318ea5c720d23e2ac8615d141a0424c
Avani Panchal <avanip.net@gmail.com>
2020-04-08 20:22:59 -0500

    FileHandlingSample

---
+++ CreateFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CreateFile
+{
+    class createDir
+    {
+        public void CreateDirectory()
+        {
+            if(Directory.Exists(@"D:/Avanee"))
+            {
+                Console.WriteLine("Directory is already created");
+                Console.WriteLine(Directory.GetCreationTime(@"D:/Avanee"));
+            }
+            else
+            {
+                Directory.CreateDirectory(@"D:/Avanee");
+                Console.WriteLine("Directory is created");
+                Console.WriteLine(Directory.GetCreationTime(@"D:/Avanee"));
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            createDir cd = new createDir();
+            cd.CreateDirectory();
+            Console.ReadLine();
+        }
+    }
+}
