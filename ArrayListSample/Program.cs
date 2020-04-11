925cfea91882b18a4359783a9d4266ecce5dd865
Avani Panchal <avanip.net@gmail.com>
2020-04-11 15:06:27 -0500

    initial commit

---
+++ ArrayListSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayListSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add("C#");
+            arr.Add('C');
+            arr.Add(123);
+            foreach(object a in arr)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("=====Insert======");
+            arr.Insert(1, 200);
+            foreach(object a in arr)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("Index of element = " + arr.IndexOf("C#"));
+            Console.WriteLine("Count = " + arr.Count);
+            Console.WriteLine("Capacity = " + arr.Capacity);
+            Console.WriteLine("Contains = " + arr.Contains("Test"));
+            Console.WriteLine("Remove Method");
+            // arr.Remove("C#");
+            Console.ReadLine();
+        }
+    }
+}
