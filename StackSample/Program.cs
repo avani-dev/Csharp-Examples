9e9a6b3f44c032b6b1bb5f4de5057308ec422e6b
Avani Panchal <avanip.net@gmail.com>
2020-04-11 15:32:20 -0500

    Stack Sample

---
+++ StackSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace StackSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+            st.Push("C#");
+            st.Push("C");
+            st.Push(123);
+            st.Push(111.12);
+            foreach(object s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("======POP=======");
+            st.Pop();
+            foreach(object s in st)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("Constains = " + st.Contains("Text"));
+            Console.WriteLine("Count = " + st.Count);
+            Console.WriteLine("GetType = " + st.GetType());
+            Console.WriteLine("GetHashCode = " + st.GetHashCode());
+            Console.ReadLine();
+        }
+    }
+}
